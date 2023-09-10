5ded2243aa68db7c6999e26edae1d99b2d0ea78a
Zack <zinouye@gmail.com>
2023-09-10 21:29:08 +0700

    created forgot password page

--- src/pages/auth/Forgot.js
+++ src/pages/auth/Forgot.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from './auth.module.scss'
+import { AiOutlineMail } from 'react-icons/ai'
+import Card from '../../components/card/Card'
+import { Link } from 'react-router-dom'
+
 
 const Forgot = () => {
   return (
-    <div>Forgot</div>
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <AiOutlineMail size={35} color="#999" />
+          </div>
+          <h2>Forgot Password</h2>
+          <form>
+            <input type="email" placeholder="Email" required name="email"/>
+            <button type="submit" className='--btn --btn-primary --btn-block'>Reset Password</button>
+          <div className={styles.links}>
+            <p>
+            <Link to="/">Home</Link>
+            </p>
+            <p>
+            <Link to="/login">Login</Link>
+            </p>
+          </div>
+          </form>
+
+        </div>
+      </Card>
+    </div>
   )
 }
 
