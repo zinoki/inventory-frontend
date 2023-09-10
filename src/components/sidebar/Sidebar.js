c028e80afb7eba3f851f7058b1fe8547b1c75463
Zack <zinouye@gmail.com>
2023-09-11 00:45:48 +0700

    worked on sidebar component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.scss'
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { RiProductHuntLine } from 'react-icons/ri'
+import menu from '../../data/sidebar'
+import SideBarItem from './SidebarItem'
 
 const Sidebar = ({children}) => {
+  const [isOpen, setIsOpen] = useState(true)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className='layout'>
-      <div className="sidebar">
-        <h2>Sidebar</h2>
+      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px"}}>
+        <div className="top_section">
+          <div className="logo" style={{ display: isOpen ? "block" : "none"}}>
+            <RiProductHuntLine size={35} style={{cursor: "pointer"}}/>
+          </div>
+          <div className="bars" style={{ marginLeft: isOpen ? "100px" : "0px"}}>
+            <HiMenuAlt3 onClick={toggle} />
+          </div>
+        </div>
       </div>
       <main>
         {children}
