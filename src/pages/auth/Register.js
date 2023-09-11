9ef5528e26eff81fbb1e1039916d26dce73bc984
Zack <zinouye@gmail.com>
2023-09-11 14:37:32 +0700

    worked on register user

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import styles from './auth.module.scss'
 import { TiUserAddOutline } from 'react-icons/ti'
 import Card from '../../components/card/Card'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { registerUser, validateEmail } from '../../services/authService'
 import { toast } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import { SET_LOGIN, SET_NAME } from '../../redux/features/auth/authSlice'
 
 const initialState = {
   name: '',
@@ -14,6 +16,8 @@ const initialState = {
 }
 
 const Register = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState(false)
   const [formData, setFormData] = useState(initialState)
   const { name, email, password, password2 } = formData
@@ -43,7 +47,10 @@ const Register = () => {
     setIsLoading(true)
     try {
       const data = await registerUser(userData)
-      console.log(data)
+      // console.log(data)
+      await dispatch(SET_LOGIN(true))
+      await dispatch(SET_NAME(data.name))
+      navigate("/dashboard")
       setIsLoading(false)
     } catch (error) {
       setIsLoading(false)
