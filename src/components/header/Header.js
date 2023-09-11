6f023a0f3820e18b99592fd467c82f421d6a9c3e
Zack <zinouye@gmail.com>
2023-09-11 16:02:58 +0700

    display name of user

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { logoutUser } from '../../services/authService'
-import { SET_LOGIN } from '../../redux/features/auth/authSlice'
-import { useDispatch } from 'react-redux'
+import { SET_LOGIN, selectName } from '../../redux/features/auth/authSlice'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+
 
 const Header = () => {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const name = useSelector(selectName)
   const logout = async () => {
     await logoutUser()
     await dispatch(SET_LOGIN(false))
@@ -18,7 +21,7 @@ const Header = () => {
       <div className="--flex-between">
         <h3>
           <span className='--fw-thin'>Welcome, </span>
-          <span className='--color-danger'>User</span>
+          <span className='--color-danger'>{name}</span>
         </h3>
         <button onClick={logout} className="--btn --btn-danger">Logout</button>
       </div>
