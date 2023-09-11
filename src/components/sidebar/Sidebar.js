c22d933de02e2045906ecfa48df2c92d688cc11a
Zack <zinouye@gmail.com>
2023-09-11 12:16:15 +0700

    navigate to home page from dashboard

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,18 +5,25 @@ import { RiProductHuntLine } from 'react-icons/ri'
 import menu from '../../data/sidebar'
 import SideBarItem from './SidebarItem'
 import SidebarItem from './SidebarItem'
+import { useNavigate } from 'react-router-dom'
+
 
 const Sidebar = ({children}) => {
   const [isOpen, setIsOpen] = useState(true)
   const toggle = () => {
     setIsOpen(!isOpen)
   }
+  const navigate = useNavigate()
+
+  const goHome = () => {
+    navigate('/')
+  }
   return (
     <div className='layout'>
       <div className="sidebar" style={{ width: isOpen ? "230px" : "60px"}}>
         <div className="top_section">
           <div className="logo" style={{ display: isOpen ? "block" : "none"}}>
-            <RiProductHuntLine size={35} style={{cursor: "pointer"}}/>
+            <RiProductHuntLine size={35} style={{cursor: "pointer"}} onClick={goHome}/>
           </div>
           <div className="bars" style={{ marginLeft: isOpen ? "100px" : "0px"}}>
             <HiMenuAlt3 onClick={toggle} />
