9d20279d8ffa28a1210bf3e3b7fa09b9a3a4cd84
Zack <zinouye@gmail.com>
2023-09-11 17:00:52 +0700

    hide or show nav menu

---
+++ src/components/protect/HiddenLink.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
+
+
+export const ShowOnLogin = ({children}) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn)
+
+  if (isLoggedIn) {
+    return <>{children}</>
+  } else {
+    return null
+  }
+}
+
+export const ShowOnLogout = ({children}) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn)
+
+  if (!isLoggedIn) {
+    return <>{children}</>
+  } else {
+    return null
+  }
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import {RiProductHuntLine} from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 import "./Home.scss"
 import heroImg from "../../assets/inv-img.png"
+import { ShowOnLogin, ShowOnLogout } from '../../components/protect/HiddenLink'
 
 const Home = () => {
   return (
@@ -12,19 +13,25 @@ const Home = () => {
           <RiProductHuntLine size={35} />
         </div>
         <ul className="home-links">
+          <ShowOnLogout>
           <li>
             <Link to="/register">Register</Link>
           </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
           <li>
             <button className="--btn --btn-primary">
             <Link to="/login">Login</Link>
             </button>
           </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
           <li>
             <button className="--btn --btn-primary">
               <Link to="/dashboard">Dashboard</Link>
             </button>
           </li>
+          </ShowOnLogin>
         </ul>
       </nav>
       {/* Hero Section */}
