6420d00d4e3a7fdb9242d50e443ecff0c1605dfa
Zack <zinouye@gmail.com>
2023-09-11 14:57:22 +0700

    create login user function

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,6 +3,8 @@ import { toast } from 'react-toastify'
 
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 
+
+// Register User
 export const registerUser = async (userData) => {
   try {
     const response = await axios.post(`${BACKEND_URL}/api/users/register`, userData, {withCredentials: true})
@@ -15,6 +17,21 @@ export const registerUser = async (userData) => {
     toast.error(message)
   }
 }
+
+// Login User
+export const loginUser = async (userData) => {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/api/users/login`, userData)
+    if (response.statusText === "OK") {
+      toast.success("Login successful")
+    }
+    return response.data
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+    toast.error(message)
+  }
+}
+
 
 export const validateEmail = (email) => {
   return email.match(
