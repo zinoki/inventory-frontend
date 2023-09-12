b5f7c070c08d047392886247ad145af3e6396b1e
Zack <zinouye@gmail.com>
2023-09-12 22:39:20 +0700

    getting login status

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Layout from './components/layout/Layout'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getLoginStatus } from "./services/authService";
+import { SET_LOGIN } from "./redux/features/auth/authSlice";
 
 axios.defaults.withCredentials = true
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus()
+      dispatch(SET_LOGIN(status))
+    }
+    loginStatus()
+  }, [dispatch])
   return (
     <BrowserRouter>
     <ToastContainer />
--- src/services/authService.js
+++ src/services/authService.js
@@ -74,6 +74,21 @@ export const resetPassword = async (userData, resetToken) => {
   }
 }
 
+// Get login status
+export const getLoginStatus = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`)
+    return response.data
+  } catch (error) {
+    const message =
+    (error.response && error.response.data && error.response.data.message) ||
+    error.message ||
+    error.toString();
+  toast.error(message);
+  }
+}
+
+
 export const validateEmail = (email) => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
