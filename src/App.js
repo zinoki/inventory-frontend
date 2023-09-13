ddd6e4a4fede94c2ca07b311b65d68084f99224c
Zack <zinouye@gmail.com>
2023-09-13 13:46:07 +0700

    created add product route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getLoginStatus } from "./services/authService";
 import { SET_LOGIN } from "./redux/features/auth/authSlice";
+import AddProduct from "./pages/addProduct/AddProduct";
 
 axios.defaults.withCredentials = true
 
@@ -43,6 +44,14 @@ function App() {
           </Layout>
         </Sidebar>
       } />
+      <Route path="/add-product" element={
+        <Sidebar>
+          <Layout>
+            <AddProduct />
+          </Layout>
+        </Sidebar>
+      } />
+        
     </Routes>
     </BrowserRouter>
   );
