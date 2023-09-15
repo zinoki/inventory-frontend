3dda2ef007f6ce98205e6761ef08387ea856f454
Zack <zinouye@gmail.com>
2023-09-15 23:28:19 +0700

    create get user profile function

--- src/services/authService.js
+++ src/services/authService.js
@@ -88,6 +88,20 @@ export const getLoginStatus = async () => {
   }
 }
 
+// Get User Profile
+export const getUser = async() => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/getuser`)
+    return response.data
+  } catch (error) {
+    const message =
+    (error.response && error.response.data && error.response.data.message) ||
+    error.message ||
+    error.toString();
+  toast.error(message);
+  }
+}
+
 
 export const validateEmail = (email) => {
   return email.match(
