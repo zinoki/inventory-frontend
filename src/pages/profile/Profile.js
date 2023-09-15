e477851bdfe6c3ebc6502f4f2e838a2c49ad0293
Zack <zinouye@gmail.com>
2023-09-15 23:52:09 +0700

    added profile component logic jsx

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Profile.scss'
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser'
+import { useDispatch } from 'react-redux'
+import { getUser } from '../../services/authService'
+import { SET_NAME, SET_USER } from '../../redux/features/auth/authSlice'
 
 const Profile = () => {
+  useRedirectLoggedOutUser('/login')
+  const dispatch = useDispatch()
+
+  const [profile, setProfile] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    async function getUserData() {
+      const data = await getUser()
+      console.log(data);
+
+      setProfile(data)
+      setIsLoading(false)
+      await dispatch(SET_USER(data))
+      await dispatch(SET_NAME(data.name))
+    }
+    getUserData()
+  }, [dispatch])
+
+
   return (
     <div>Profile</div>
   )
