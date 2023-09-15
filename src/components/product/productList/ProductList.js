79b4a18ffadd20014e51f450b659f7f49181e795
Zack <zinouye@gmail.com>
2023-09-15 15:02:02 +0700

    created search function in redux

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductList.scss'
 import { SpinnerImg } from '../../loader/Loader'
 import {FaEdit, FaTrashAlt} from 'react-icons/fa'
 import { AiOutlineEye } from 'react-icons/ai'
 import Search from '../../search/Search'
+import { useDispatch, useSelector } from 'react-redux'
+import { FILTER_PRODUCTS, selectFilteredProducts } from '../../../redux/features/product/filterSlice'
+
 
 const ProductList = ({products, isLoading}) => {
   const [search, setSearch] = useState('')
+  const filteredProducts = useSelector(selectFilteredProducts)
+
+  const dispatch = useDispatch()
   const shortenText = (text, n) => {
     if(text.length > n) {
       const shortenedText = text.substring(0,n).concat("...")
@@ -15,6 +21,9 @@ const ProductList = ({products, isLoading}) => {
     return text
   }
 
+  useEffect(() => {
+    dispatch(FILTER_PRODUCTS({products, search}))
+  }, [products, search, dispatch])
   return (
   <div className='product-list'>
     <hr />
@@ -45,7 +54,7 @@ const ProductList = ({products, isLoading}) => {
               </tr>
             </thead>
             <tbody>
-              {products.map((product, index) => {
+              {filteredProducts.map((product, index) => {
                 const {_id, name, category, price, quantity} = product
                 return (
                   <tr key={_id}>
