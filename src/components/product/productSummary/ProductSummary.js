311f90e5c9dc094d4418b2f3785080a24c869f7a
Zack <zinouye@gmail.com>
2023-09-15 15:54:33 +0700

    product summary jsx

--- src/components/product/productSummary/ProductSummary.js
+++ src/components/product/productSummary/ProductSummary.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './ProductSummary.scss'
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BsCart4, BsCartX } from "react-icons/bs";
+import { BiCategory } from "react-icons/bi";
+import InfoBox from '../../infoBox/InfoBox';
 
-const ProductSummary = () => {
+// Icons
+const earningIcon = <AiFillDollarCircle size={40} color="#fff" />;
+const productIcon = <BsCart4 size={40} color="#fff" />;
+const categoryIcon = <BiCategory size={40} color="#fff" />;
+const outOfStockIcon = <BsCartX size={40} color="#fff" />;
+
+
+const ProductSummary = ({products}) => {
   return (
-    <div>ProductSummary</div>
+    <div className='product-summary'>
+      <h3 className='--mt'>Inventory Stats</h3>
+      <div className="info-summary">
+        <InfoBox icon={productIcon} title={"Total Products"} count={products.length} bgColor="card1"/>
+        <InfoBox icon={earningIcon} title={"Total Store Value"} count={"0"} bgColor="card2"/>
+        <InfoBox icon={outOfStockIcon} title={"Out of Stock"} count={"0"} bgColor="card3"/>
+        <InfoBox icon={categoryIcon} title={"All Categories"} count={"0"} bgColor="card4"/>
+      </div>
+    </div>
   )
 }
 
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { selectIsLoggedIn } from '../../redux/features/auth/authSlice'
 import { getProducts } from '../../redux/features/product/productSlice'
 import ProductList from '../../components/product/productList/ProductList'
+import ProductSummary from '../../components/product/productSummary/ProductSummary'
 
 
 const Dashboard = () => {
@@ -27,7 +28,8 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h2>Dashboard</h2>
+
+      <ProductSummary products={products}/>
       <ProductList products={products} isLoading={isLoading} />
     </div>
   )
