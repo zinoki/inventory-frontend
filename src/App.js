b6e141d59b1f691c16373a7e500f4721da8ea160
Zack <zinouye@gmail.com>
2023-09-15 17:39:59 +0700

    created edit product route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import { getLoginStatus } from "./services/authService";
 import { SET_LOGIN } from "./redux/features/auth/authSlice";
 import AddProduct from "./pages/addProduct/AddProduct";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
+import EditProduct from "./pages/editProduct/EditProduct";
 
 
 axios.defaults.withCredentials = true
@@ -57,6 +58,13 @@ function App() {
         <Sidebar>
           <Layout>
             <ProductDetails />
+          </Layout>
+        </Sidebar>
+      } />
+      <Route path="/edit-product/:id" element={
+        <Sidebar>
+          <Layout>
+            <EditProduct />
           </Layout>
         </Sidebar>
       } />
