5146ec9364ee4032a8e5aecdb79ac0b1cbe046a8
Zack <zinouye@gmail.com>
2023-09-15 23:32:52 +0700

    create profile component and route

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import { SET_LOGIN } from "./redux/features/auth/authSlice";
 import AddProduct from "./pages/addProduct/AddProduct";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
 import EditProduct from "./pages/editProduct/EditProduct";
+import Profile from "./pages/profile/Profile";
 
 
 axios.defaults.withCredentials = true
@@ -65,6 +66,13 @@ function App() {
         <Sidebar>
           <Layout>
             <EditProduct />
+          </Layout>
+        </Sidebar>
+      } />
+      <Route path="/profile" element={
+        <Sidebar>
+          <Layout>
+            <Profile />
           </Layout>
         </Sidebar>
       } />
